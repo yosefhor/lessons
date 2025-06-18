89ec226d2ebb97c8da25134f0d55de64f17cd9dc
yossef horvitz work pc <yossefhorvitz@gmail.com>
2025-06-18 22:59:51 +0300

    3.5_completed

--- lesson_1/3Fetching/fetch.js
+++ lesson_1/3Fetching/fetch.js
@@ -1,9 +1,9 @@
 const url = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT';
 
-const fetch = async (url) => {
+const fetchBinance = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
 }
 
-fetch(url)
+fetchBinance(url)
--- lesson_1/4TodoList/toDoList.js
+++ lesson_1/4TodoList/toDoList.js
@@ -1,14 +1,28 @@
 import fs from 'fs';
-import readline from 'readline';
 
-function add(task){
+const jsonFile = 'todoList.json'
+
+async function checkFileExist () {
+    return fs.existsSync('todoList.json');
+}
+
+async function add(task) {
+    const fileExist = checkFileExist();
+    if (!fileExist) await fs.promises.writeFile(jsonFile, {}, 'utf8')
+    await fs.promises.appendFile(jsonFile, task)
+}
+
+function remove(task) {
 
 }
 
-function remove(task){
-
+async function list() {
+    const response = await fs.promises.readFile(jsonFile, 'utf8');
+    const parsed = await JSON.parse(response)
+    return parsed
 }
 
-function list(){
-
+const handleError = (errMessage) => {
+    console.log(`Error: ${errMessage}`);
 }
+export{add, remove, list}
