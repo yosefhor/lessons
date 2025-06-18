fdcce1382623a26c124fef860c5abd5eaa98bc1f
yossef horvitz work pc <yossefhorvitz@gmail.com>
2025-06-19 00:07:56 +0300

    4_completed

--- lesson_1/4TodoList/toDoList.js
+++ lesson_1/4TodoList/toDoList.js
@@ -1,28 +1,37 @@
 import fs from 'fs';
 
-const jsonFile = 'todoList.json'
+const jsonFile = 'todoList.json';
+const fn = process.argv[2];
+const task = process.argv.slice(3).join(' ');
 
-async function checkFileExist () {
+function checkFileExist() {
     return fs.existsSync('todoList.json');
 }
 
 async function add(task) {
     const fileExist = checkFileExist();
-    if (!fileExist) await fs.promises.writeFile(jsonFile, {}, 'utf8')
-    await fs.promises.appendFile(jsonFile, task)
+    if (!fileExist) {
+        await fs.promises.writeFile(jsonFile, JSON.stringify([task]), 'utf8')
+    } else {
+        const tasks = await list()
+        tasks.push(task)
+        await fs.promises.writeFile(jsonFile, JSON.stringify(tasks), 'utf8')
+    }
 }
 
-function remove(task) {
-
+async function remove(task) {
+    const prevTasks = await list()
+    const updatedTasks = prevTasks.filter(t => t !== task)
+    await fs.promises.writeFile(jsonFile, JSON.stringify(updatedTasks), 'utf8')
 }
 
 async function list() {
     const response = await fs.promises.readFile(jsonFile, 'utf8');
-    const parsed = await JSON.parse(response)
-    return parsed
+    const parsed = JSON.parse(response)
+    console.log(parsed);
 }
 
-const handleError = (errMessage) => {
-    console.log(`Error: ${errMessage}`);
-}
-export{add, remove, list}
+// const handleError = (errMessage) => {
+//     console.log(`Error: ${errMessage}`);
+// }
+fn === 'add' ? add(task) : fn === 'remove' ? remove(task) : list()
