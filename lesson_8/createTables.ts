51fa4f92db58056d561788ae1a7b47478f23a958
Yossef Horvitz <yossefhorvitz@gmail.com>
2025-06-29 23:54:44 +0300

    .

--- lesson_8/createTables.ts
+++ lesson_8/createTables.ts
@@ -1,5 +1,6 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 import { CreateTableCommand, CreateTableCommandInput } from "@aws-sdk/client-dynamodb";
-import { client } from "./dynamoClient";
+import { client } from "./lib/dynamoClient.ts";
 
 async function createTable(params: CreateTableCommandInput) {
   try {
