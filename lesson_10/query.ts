0c5e152145ff1eeb49d402b163d8bc46d45f864e
yossef horvitz work pc <yossefhorvitz@gmail.com>
2025-07-03 10:29:51 +0300

    .

--- lesson_10/query.ts
+++ lesson_10/query.ts
@@ -2,25 +2,27 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 import { client } from "./lib/dynamoClient.ts";
 import { QueryCommand } from "@aws-sdk/lib-dynamodb";
 
-async function createTable() {
+async function runQuery() {
     try {
-        const tableName: string = "UsersWithGSI";
+        const tableName: string = "users_GSI";
         // const indexName: string = "";
         const result: any[] = [];
         const start: number = performance.now();
-        for (let i = 1; i < 100; i++) {
+        // for (let i = 1; i < 100; i++) {
             const response: any = await client.send(new QueryCommand({
                 TableName: tableName,
-                // IndexName: indexName,
-                KeyConditionExpression: 'userID = :id',
+                IndexName: 'RegistrationDate_index',
+                KeyConditionExpression: 'userID = :uid AND RegistrationDate BETWEEN :start AND :end',
                 ExpressionAttributeValues: {
-                    ':id': 'U001'
+                    ':uid': 'U001',
+                    ':start': '2025-07-02T20:00:46.516Z',
+                    ':end': '2025-07-02T20:01:46.494Z'
                 }
             }));
             result.push(response.Items)
-        }
+        // }
         console.log(`Time for the process: ${performance.now() - start}`);
-        console.log(`✅`, result);
+        console.log(`✅`);
     } catch (err: any) {
         if (err.name === "ResourceInUseException") {
             console.log(`⚠️ already exists:`);
@@ -30,4 +32,4 @@ async function createTable() {
     }
 }
 
-createTable();
+runQuery();
